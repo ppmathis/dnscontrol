4b3a857d7c720ecd24cbe146dae23ece340f9d75
Pascal Mathis <dev@ppmathis.com>
2026-07-24 19:15:59 +0200

    BUNNY_DNS: Fix NS TTL record handling for DNSControl v5

--- providers/bunnydns/convert.go
+++ providers/bunnydns/convert.go
@@ -17,16 +17,10 @@ var fqdnTypes = []recordType{recordTypeCNAME, recordTypeHTTPS, recordTypeMX, rec
 var nullTypes = []recordType{recordTypeHTTPS, recordTypeMX, recordTypeSVCB}
 
 func fromRecordConfig(rc *models.RecordConfig) (*record, error) {
-
-	ttl := rc.TTL
-	if rc.Type == "NS" {
-		ttl = 0
-	}
-
 	r := record{
 		Type: recordTypeFromString(rc.Type),
 		Name: rc.GetLabel(),
-		TTL:  ttl,
+		TTL:  rc.TTL,
 	}
 
 	switch r.Type {
